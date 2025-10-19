c3a26cf16dfec7d35673a015b20a3e3393f9d8fa
josseycodes1 <adewumijosephine1@gmail.com>
2025-10-19 09:47:58 +0100

    api customized view added

--- profileapi/urls.py
+++ profileapi/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import ProfileView
+from rest_framework.documentation import include_docs_urls
 
 urlpatterns = [
     path('me', ProfileView.as_view(), name='profile'),
+    path('docs/', include_docs_urls(title='Profile API')),
 ]
